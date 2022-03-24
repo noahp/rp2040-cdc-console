2109610257718ee995c46016481ee8a058fea459
Noah Pendleton <2538614+noahp@users.noreply.github.com>
2022-03-24 15:18:45 -0400

    fix shellcheck warning

--- docker-build.sh
+++ docker-build.sh
@@ -18,6 +18,7 @@ DOCKER_BUILDKIT=1 docker build -t "$DOCKER_IMAGE_NAME" --build-arg "UID=$(id -u)
 # include the -i and prompt if running in interactive mode
 if [ -t 0 ] && [ -t 1  ]; then
     TTY_ARG="-i"
+    # shellcheck disable=SC2016
     TTY_EXTRA=' && read -p ">>> Run '"'"'docker cp $(cat /etc/hostname):/tmp/build/rp2040_cdc_console.uf2 ./'"'"' to save the built UF2 file, then press ENTER"'
 fi
 docker run --rm ${TTY_ARG} -v "$(pwd)":/workspace --workdir /workspace -t rp2040-cdc-console \
